d3b4bff73d62f69153323af0fb41bbc91eae438c
MohammadrezaShahbazi <EMINEMENGLAND2000@GMAIL.com>
2025-12-04 20:41:43 +0330

    Update script.js

--- script.js
+++ script.js
@@ -121,6 +121,10 @@ const messages = {
         riskMessage_low: 'Low risk',
         riskMessage_mid: 'Moderate risk — check-up recommended',
         riskMessage_high: 'High risk — medical consultation advised',
+         riskMessage_low: 'Low risk',
+        riskMessage_mid: 'Moderate risk — check-up recommended',
+        riskMessage_high: 'High risk — medical consultation advised',
+        attentionCheckup: '<strong>ATTENTION:</strong> This tool is solely a predictive model based on statistical data and is never a substitute for a specialist physician\'s diagnosis. <strong>Please take annual checkups and tests seriously and consult your doctor to ensure your full health status.</strong>',
     }
 };
 
